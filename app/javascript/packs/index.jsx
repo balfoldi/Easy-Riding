e3ba52713313419800947b2ab1d8b4a2ca4ffa69
Lucas <lucas.gautier@live.fr>
2020-12-10 14:39:49 +0100

    scroll up fix

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -12,7 +12,7 @@ const App = () => {
     <div>
       <NavMain />
       <BreadCrumb />
-      <ScrollToTop smooth showUnder={190}/>
+      <ScrollToTop/>
       <Home />
       <FooterMain />
     </div>
