6963e12c689e735fad21475d21cd9f019f53feba
balfoldi <balfoldi@yahoo.fr>
2020-12-11 15:40:32 +0100

    fix bug

--- app/javascript/packs/components/NavMain/index.jsx
+++ app/javascript/packs/components/NavMain/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component, useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import { Nav, NavDropdown } from "react-bootstrap";
 import "./navmain.scss";
@@ -18,41 +18,53 @@ const NavMain = () => {
 
   if (isAuthenticated) {
     return (
-      <React.Fragment >
+      <React.Fragment>
         <HeaderImage />
-          <Navbar id="nav-main" collapseOnSelect expand="lg">
-            <Navbar.Brand id="title-main" href="#">
-            <Link to='/'>Easy Riding</Link>
+        <Navbar id="nav-main" collapseOnSelect expand="lg">
+          <Navbar.Brand id="title-main" href="#">
+            <Link to="/">Easy Riding</Link>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav id="right-align">
               <Nav className="mr-auto">
                 <NavDropdown title="Menu" id="collasible-nav-dropdown">
-                    <Link to='/mon-compte' id="item-link">MON COMPTE</Link>
+                  <Link to="/mon-compte" id="item-link">
+                    MON COMPTE
+                  </Link>
                   <NavDropdown.Divider />
-                    <Link to='/mon-compte/mon-garage' id="item-link">Mon Garage</Link>
+                  <Link to="/mon-compte/mon-garage" id="item-link">
+                    Mon Garage
+                  </Link>
                   <NavDropdown.Divider />
-                  <Link to='/mon-compte/mes-annonces' id="item-link">Mes Annonces</Link>
+                  <Link to="/mon-compte/mes-annonces" id="item-link">
+                    Mes Annonces
+                  </Link>
                   <NavDropdown.Divider />
-                    <Link to='/mon-compte/mes-réservations' id="item-link">Mes Réservations</Link>
+                  <Link to="/mon-compte/mes-réservations" id="item-link">
+                    Mes Réservations
+                  </Link>
                   <NavDropdown.Divider />
-                    <Link to='/mon-compte/mes-favoris' id="item-link">Mes Favoris</Link>
+                  <Link to="/mon-compte/mes-favoris" id="item-link">
+                    Mes Favoris
+                  </Link>
                 </NavDropdown>
                 <Nav.Link onClick={disconnectUser} className="auth-links">
                   Déconnexion
                 </Nav.Link>
               </Nav>
-            </Navbar.Collapse>
-          </Navbar>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
       </React.Fragment>
     );
-  } return (
-   <React.Fragment >
-    <HeaderImage />
+  }
+  return (
+    <React.Fragment>
+      <HeaderImage />
       <Navbar id="nav-main" collapseOnSelect expand="lg">
         <Navbar.Brand id="title-main" href="#">
-          <Link to='/'>Easy Riding</Link>
+          <Link to="/">Easy Riding</Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
