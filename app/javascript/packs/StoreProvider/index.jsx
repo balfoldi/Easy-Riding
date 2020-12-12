00dcef057d9b46c32b8f315c2c03eb6e5e04f4c1
balfoldi <balfoldi@yahoo.fr>
2020-12-12 13:47:15 +0100

    drying code

--- app/javascript/packs/StoreProvider/index.jsx
+++ app/javascript/packs/StoreProvider/index.jsx
@@ -1,11 +1,10 @@
-import React, { createContext, useContext } from "react";
-import { useLocalObservable, Observer } from "mobx-react";
+import React, { createContext } from "react";
+import { useLocalObservable } from "mobx-react";
 import Cookies from "js-cookie";
 
 export const StoreContext = createContext();
 
 const StoreProvider = ({ children }) => {
-  console.log("StoreProvider({ children }")
   const store = useLocalObservable(() => ({
     currentUser: Cookies.get("currentUser") ? JSON.parse(Cookies.get("currentUser")) : null,
     setCurrentUser: (response) => {
