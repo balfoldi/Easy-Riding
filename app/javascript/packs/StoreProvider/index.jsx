f545d5b2e9a955b94e27060b5ce59a65936ef231
balfoldi <balfoldi@yahoo.fr>
2020-12-12 13:23:18 +0100

    mobx files implementeds

---
+++ app/javascript/packs/StoreProvider/index.jsx
@@ -0,0 +1,19 @@
+import React, { createContext, useContext } from "react";
+import { useLocalObservable, Observer } from "mobx-react";
+import Cookies from "js-cookie";
+
+export const StoreContext = createContext();
+
+const StoreProvider = ({ children }) => {
+  console.log("StoreProvider({ children }")
+  const store = useLocalObservable(() => ({
+    currentUser: Cookies.get("currentUser") ? JSON.parse(Cookies.get("currentUser")) : null,
+    setCurrentUser: (response) => {
+      store.currentUser = response;
+    },
+  }));
+///
+  return <StoreContext.Provider value={store}>{children}</StoreContext.Provider>;
+};
+
+export default StoreProvider;
