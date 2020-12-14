45cdddfc13ef911b2992f38c086a2355e4598f3a
Lucas <lucas.gautier@live.fr>
2020-12-14 18:31:40 +0100

    front typos et couleurs

--- app/javascript/packs/components/layout/NavMain/index.jsx
+++ app/javascript/packs/components/layout/NavMain/index.jsx
@@ -1,7 +1,7 @@
 import "./index.scss";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Nav, Navbar, NavDropdown, Dropdown } from "react-bootstrap";
+import { Nav, Navbar, NavDropdown, Dropdown, Container } from "react-bootstrap";
 
 const NavMain = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(true);
@@ -16,66 +16,70 @@ const NavMain = () => {
 
   if (isAuthenticated) {
     return (
+      <Container>
+        <Navbar id="nav-main" collapseOnSelect expand="lg">
+          <Navbar.Brand id="title-main">
+            <Link className="logo" to="/">Easy Riding</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav id="right-align">
+              <Nav className="mr-auto">
+                <NavDropdown title="Menu" id="collasible-nav-dropdown">
+                  <Dropdown.Item as={Link} to="/mon-compte" id="item-link">
+                    MON COMPTE
+                  </Dropdown.Item>
+                  <NavDropdown.Divider />
+
+                  <Dropdown.Item as={Link} to="/mon-compte/mon-garage" id="item-link">
+                    Mon Garage
+                  </Dropdown.Item>
+                  <NavDropdown.Divider />
+
+                  <Dropdown.Item as={Link} to="/mon-compte/mes-annonces" id="item-link">
+                    Mes Annonces
+                  </Dropdown.Item>
+                  <NavDropdown.Divider />
+
+                  <Dropdown.Item as={Link} to="/mon-compte/mes-réservations" id="item-link">
+                    Mes Réservations
+                  </Dropdown.Item>
+                  <NavDropdown.Divider />
+
+                  <Dropdown.Item as={Link} to="/mon-compte/mes-favoris" id="item-link">
+                    Mes Favoris
+                  </Dropdown.Item>
+                </NavDropdown>
+                <Nav.Link onClick={disconnectUser} className="auth-links">
+                  Déconnexion
+                </Nav.Link>
+              </Nav>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+    );
+  }
+  return (
+    <Container>
       <Navbar id="nav-main" collapseOnSelect expand="lg">
-        <Navbar.Brand id="title-main">
-          <Link to="/">Easy Riding</Link>
+        <Navbar.Brand id="title-main" href="#">
+          <Link className="logo" to="/">Easy Riding</Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav id="right-align">
-            <Nav className="mr-auto">
-              <NavDropdown title="Menu" id="collasible-nav-dropdown">
-                <Dropdown.Item as={Link} to="/mon-compte" id="item-link">
-                  MON COMPTE
-                </Dropdown.Item>
-                <NavDropdown.Divider />
+            <Nav.Link onClick={connectUser} className="auth-links" href="#">
+              Connexion
+            </Nav.Link>
 
-                <Dropdown.Item as={Link} to="/mon-compte/mon-garage" id="item-link">
-                  Mon Garage
-                </Dropdown.Item>
-                <NavDropdown.Divider />
-
-                <Dropdown.Item as={Link} to="/mon-compte/mes-annonces" id="item-link">
-                  Mes Annonces
-                </Dropdown.Item>
-                <NavDropdown.Divider />
-
-                <Dropdown.Item as={Link} to="/mon-compte/mes-réservations" id="item-link">
-                  Mes Réservations
-                </Dropdown.Item>
-                <NavDropdown.Divider />
-
-                <Dropdown.Item as={Link} to="/mon-compte/mes-favoris" id="item-link">
-                  Mes Favoris
-                </Dropdown.Item>
-              </NavDropdown>
-              <Nav.Link onClick={disconnectUser} className="auth-links">
-                Déconnexion
-              </Nav.Link>
-            </Nav>
+            <Nav.Link className="auth-links inscription-navbar" href="#pricing">
+              Inscription
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-    );
-  }
-  return (
-    <Navbar id="nav-main" collapseOnSelect expand="lg">
-      <Navbar.Brand id="title-main" href="#">
-        <Link to="/">Easy Riding</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav id="right-align">
-          <Nav.Link onClick={connectUser} className="auth-links" href="#">
-            Connexion
-          </Nav.Link>
-
-          <Nav.Link className="auth-links" href="#pricing">
-            Inscription
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    </Container>
   );
 };
 
--- app/javascript/packs/pages/Home/Jumbo/index.jsx
+++ app/javascript/packs/pages/Home/Jumbo/index.jsx
@@ -6,17 +6,17 @@ import France from "./france.png";
 
 const Jumbo = () => {
   return (
-    <Jumbotron fluid id="jumbotron">
-      <div id="info">
-        <p id="presentation">Visiter la France sur la moto de mes rêves ? </p>
-        <Link to="/annonces" id="home-link">
-          <Button variant="contained" id="home-button"><span>C'est parti</span></Button>
-        </Link>
-      </div>
-      <div id="map">
-        <Image src={France} />
-      </div>
-    </Jumbotron>
+      <Jumbotron fluid id="jumbotron">
+        <div id="info">
+          <p id="presentation">Visiter la France sur la moto de mes rêves ? </p>
+          <Link to="/annonces" id="home-link">
+            <Button className="ButtonHomepage" variant="contained" id="home-button"><span>C'est parti</span></Button>
+          </Link>
+        </div>
+        <div id="map">
+          <Image src={France} />
+        </div>
+      </Jumbotron>
   )
 }
 
