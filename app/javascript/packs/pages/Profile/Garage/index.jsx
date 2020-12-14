cb3f3b09e480a3a6cb438bc1f5ad62d8d0d6c26f
balfoldi <balfoldi@yahoo.fr>
2020-12-14 03:29:07 +0100

    fixed no-bike crash

--- app/javascript/packs/pages/Profile/Garage/index.jsx
+++ app/javascript/packs/pages/Profile/Garage/index.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import BikeFormModal from "./BikeFormModal";
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { Container, Row, Col } from "reactstrap";
-import BikeShow  from './BikeShow'
+import BikeShow from "./BikeShow";
 
 const Garage = () => {
   const [modal, setModal] = useState(false);
   const toggle = () => setModal(!modal);
   const [bikes, setBikes] = useState([]);
-  const [bike, setBike] = useState({})
+  const [bike, setBike] = useState(null);
 
   const fetchMyBikes = () => {
     fetch("/api/bikes")
@@ -16,7 +16,7 @@ const Garage = () => {
       .then((response) => {
         console.log(response);
         setBikes(response);
-        setBike(response[0])
+        setBike(response[0]);
       });
   };
 
@@ -26,28 +26,40 @@ const Garage = () => {
 
   return (
     <Container>
-      <Row>
-        <Col sm="3" className="pt-5">
-          <h4>Mes Motos</h4>
-          <hr></hr>
-          <Button color="danger" onClick={toggle}>
-            Ajouter une moto
-          </Button>
-          <hr></hr>
-          {bikes.map((bike) => (
-            <Button variant="light" onClick={()=>setBike(bike)}>{bike.model}</Button>
-          ))}
-        </Col>
-        <Col sm='9'>
-        <BikeShow bike={bike}/>
-        </Col>
-        <BikeFormModal
-          modal={modal}
-          toggle={toggle}
-          fetchMyBikes={fetchMyBikes}
-          setModal={setModal}
-        />
-      </Row>
+      {bike ? (
+        <Row>
+          <Col sm="3" className="pt-5">
+            <h4>Mes Motos</h4>
+            <hr></hr>
+            <Button color="danger" onClick={toggle}>
+              Ajouter une moto
+            </Button>
+            <hr></hr>
+            {bikes.map((bike) => (
+              <Button variant="light" onClick={() => setBike(bike)}>
+                {bike.model}
+              </Button>
+            ))}
+          </Col>
+          <Col sm="9">
+            <BikeShow bike={bike} />
+          </Col>
+        </Row>
+      ) : (
+        <Card className="my-5">
+          <Card.Body>
+            <Button onClick={toggle} className="w-100">
+              Ajoute ta première moto!
+            </Button>
+          </Card.Body>
+        </Card>
+      )}
+      <BikeFormModal
+        modal={modal}
+        toggle={toggle}
+        fetchMyBikes={fetchMyBikes}
+        setModal={setModal}
+      />
     </Container>
   );
 };
