fee79c10b940b927f8b7ba0edfc64649e4bb1e5e
hsravo <hsravo1@gmail.com>
2020-12-14 16:48:08 +0100

    bikecard design ok

--- app/javascript/packs/pages/Offers/index.jsx
+++ app/javascript/packs/pages/Offers/index.jsx
@@ -5,7 +5,7 @@ import Breadcrumb from "../../components/layout/BreadCrumb";
 import SearchBar from "./SearchBar";
 import AsideBar from "./AsideBar";
 import BikeList from "./BikeList";
-import BikeCard from "./BikeList/BikeCard";
+
 
 const Offers = () => {
   return (
@@ -13,7 +13,16 @@ const Offers = () => {
       <Breadcrumb />
       <SearchBar />
       <Container>
-        <BikeCard />
+        <Row>
+          <Col sm={4}>
+            <br></br>
+            <AsideBar />
+          </Col>
+          <Col sm={8}>
+            <br></br>
+            <BikeList />
+          </Col>
+        </Row>
       </Container>
     </React.Fragment>
   )
