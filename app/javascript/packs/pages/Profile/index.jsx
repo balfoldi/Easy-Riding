aab861bb241a64828ccef0df992366642e57e9a5
hsravo <hsravo1@gmail.com>
2020-12-16 00:02:35 +0100

    fixing

--- app/javascript/packs/pages/Profile/index.jsx
+++ app/javascript/packs/pages/Profile/index.jsx
@@ -7,11 +7,7 @@ import MyOffers from "./MyOffers";
 import ProfileInfo from "./ProfileInfo"
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
-import NavProfile from "./NavProfile";
-import ProfileInfo from "./ProfileInfo";
-import NotFound from "../NotFound";
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
-import { Container } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
@@ -20,39 +16,33 @@ const Profile = () => {
 
   return (
     <div id="body">
-    <React.Fragment>
-        <Container >
-        <FontAwesomeIcon icon={faArrowRight} />
-          <Breadcrumb className="breadcrumb">
-            <div id="Introductionphrase">Vous etes ici :</div>
-            <Breadcrumb.Item href="/" className="ml-2 breadcrumb_link">Accueil</Breadcrumb.Item>
-            <Breadcrumb.Item active className="color breadcrumb_link">Mon compte</Breadcrumb.Item>
-          </Breadcrumb>
-          <Router>
-          <Tabs
-            id="controlled-tab-example"
-            activeKey={key}
-            onSelect={(k) => setKey(k)}
-          >
-            <Tab eventKey="ProfileInfo" title="Profil">
-              <div id="tab-body"><ProfileInfo /></div>
-            </Tab>
-            <Tab eventKey="Garage" title="Garage">
-              <div id="tab-body"><Garage /></div>
-            </Tab>
-            <Tab eventKey="MyOffers" title="Annonces">
-              <div id="tab-body"><MyOffers /></div>
-            </Tab>
-            <Tab eventKey="Favorites" title="Favoris">
-              <div id="tab-body"><Favorites /></div>
-            </Tab>
-            <Tab eventKey="MyBookings" title="Réservations">
-              <div id="tab-body"><MyBookings /></div>
-            </Tab>
-          </Tabs>
-         <h2>Page Profile en cours de construction...</h2>
-        </Container>
-      </React.Fragment>
+      <FontAwesomeIcon icon={faArrowRight} />
+        <Breadcrumb className="breadcrumb">
+          <div id="Introductionphrase">Vous etes ici :</div>
+          <Breadcrumb.Item href="/" className="ml-2 breadcrumb_link">Accueil</Breadcrumb.Item>
+          <Breadcrumb.Item active className="color breadcrumb_link">Mon compte</Breadcrumb.Item>
+        </Breadcrumb>
+        <Tabs
+          id="controlled-tab-example"
+          activeKey={key}
+          onSelect={(k) => setKey(k)}
+        >
+          <Tab eventKey="ProfileInfo" title="Profil">
+            <div id="tab-body"><ProfileInfo /></div>
+          </Tab>
+          <Tab eventKey="Garage" title="Garage">
+            <div id="tab-body"><Garage /></div>
+          </Tab>
+          <Tab eventKey="MyOffers" title="Annonces">
+            <div id="tab-body"><MyOffers /></div>
+          </Tab>
+          <Tab eventKey="Favorites" title="Favoris">
+            <div id="tab-body"><Favorites /></div>
+          </Tab>
+          <Tab eventKey="MyBookings" title="Réservations">
+            <div id="tab-body"><MyBookings /></div>
+          </Tab>
+        </Tabs>
     </div>
   );
 }
