e54a3aec031caccc85f078d99cfebe4c5105d377
Lucas <lucas.gautier@live.fr>
2020-12-15 02:17:37 +0100

    breadcrumb bug

--- app/javascript/packs/components/layout/BreadCrumb/index.jsx
+++ app/javascript/packs/components/layout/BreadCrumb/index.jsx
@@ -1,12 +1,19 @@
+import "./index.scss";
 import React from "react";
 import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import { Container } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { level-up-alt } from '@fortawesome/free-solid-svg-icons';
 
 const BreadCrumb = () => {
   return (
-    <Breadcrumb>
-      <Breadcrumb.Item href="/">Accueil</Breadcrumb.Item>
-      <Breadcrumb.Item active>Exemple de breadcrumb</Breadcrumb.Item>
-    </Breadcrumb>
+    <Container>
+      <Breadcrumb className="breadcrumb">
+      <FontAwesomeIcon icon={level-up-alt} />
+        <Breadcrumb.Item href="/">Accueil</Breadcrumb.Item>
+        <Breadcrumb.Item active>Exemple de breadcrumb</Breadcrumb.Item>
+      </Breadcrumb>
+    </Container>
   );
 }
 
