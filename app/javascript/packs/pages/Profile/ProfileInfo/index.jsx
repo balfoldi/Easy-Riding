c6e5655b6dea5cda76902defec9724c1e6b770ea
Maxime CASTILLO <maxime.castillo@gmail.com>
2020-12-16 21:19:50 +0100

    Ajoute chargement auto et édition du profil

--- app/javascript/packs/pages/Profile/ProfileInfo/index.jsx
+++ app/javascript/packs/pages/Profile/ProfileInfo/index.jsx
@@ -1,44 +1,59 @@
 import "./index.scss";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Button, Form, Col, Alert } from "react-bootstrap";
 import authStore from "../../../stores/Auth";
 import { observer } from "mobx-react";
 import Cookies from "js-cookie";
 
 const ProfileInfo = () => {
-  const [fetchErrors, setFetchErrors] = useState([]);
   const { user } = authStore;
   const userToken = Cookies.get("EasyRiderUserToken");
+  const [input, setInput] = useState({});
+  const [fetchErrors, setFetchErrors] = useState([]);
 
+  //TO-DO : Remplacer users/ID par user.id
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const body = {
-      user: {
-        username: e.target.username.value,
-        first_name: e.target.first_name.value,
-        last_name: e.target.last_name.value,
-        description: e.target.description.value,
-        email: e.target.email.value,
-        phone_number: e.target.phone_number.value
-      }
-    }
+  const loadProfile = () => {
+    fetch(`/api/users/1`, {
+      method: 'get',
+      headers: {
+        'Authorization': `Bearer ${userToken}`
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setInput(data);
+      })
+      .catch((error) => console.error(error));
+  }
 
-    fetch(`/api/users/10`, {
+  useEffect(() => {
+    loadProfile();
+  }, [])
+
+  const handleInputChange = (event) => {
+    setInput({
+      ...input,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  const editProfile = () => {
+    fetch(`/api/users/1`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${userToken}`
       },
-      body: JSON.stringify(body)
+      body: JSON.stringify({ user: input })
     })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.errors) {
-        setFetchErrors(data.errors);
-      }
-    })
-    .catch((error) => console.error(error));
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.errors) {
+          setFetchErrors(() => data.errors);
+        }
+      })
+      .catch((error) => console.error(error));
   }
 
   return (
@@ -46,46 +61,77 @@ const ProfileInfo = () => {
       <p id="intro">Mon profil</p>
 
       {fetchErrors.length > 0 && fetchErrors.map((error) => {
-        <Alert variant="warning">{error.detail}</Alert>
+        return <Alert key={error.detail} variant="warning">{error.detail}</Alert>
       })}
 
-      <Form onSubmit={handleSubmit}>
+      <Form>
 
         <Form.Group>
           <Form.Label>Pseudonyme</Form.Label>
           <Form.Control
-            name="username" type="text" placeholder="" />
+            onChange={handleInputChange}
+            value={input.username || ''}
+            name="username"
+            type="text"
+            placeholder=""
+          />
         </Form.Group>
 
         <Form.Group>
           <Form.Label>Prénom</Form.Label>
           <Form.Control
-            name="first_name" type="text" placeholder="" />
+            onChange={handleInputChange}
+            value={input.first_name || ''}
+            name="first_name"
+            type="text"
+            placeholder=""
+          />
         </Form.Group>
 
         <Form.Group>
           <Form.Label>Nom</Form.Label>
           <Form.Control
-            name="last_name" type="text" placeholder="" />
+            onChange={handleInputChange}
+            value={input.last_name || ''}
+            name="last_name"
+            type="text"
+            placeholder=""
+          />
         </Form.Group>
 
         <Form.Group>
           <Form.Label>Description</Form.Label>
           <Form.Control
-            name="description" as="textarea" rows={3} placeholder="Présentez-vous en quelques mots..." />
+            onChange={handleInputChange}
+            value={input.description || ''}
+            name="description"
+            as="textarea"
+            rows={3}
+            placeholder="Présentez-vous en quelques mots..."
+          />
         </Form.Group>
 
         <Form.Row>
           <Form.Group as={Col}>
             <Form.Label>Votre adresse email</Form.Label>
             <Form.Control
-              name="email" type="email" placeholder="" />
+              onChange={handleInputChange}
+              value={input.email || ''}
+              name="email"
+              type="email"
+              placeholder=""
+            />
           </Form.Group>
 
           <Form.Group as={Col}>
             <Form.Label>Numéro de téléphone</Form.Label>
             <Form.Control
-              name="phone_number" type="text" placeholder="" />
+              onChange={handleInputChange}
+              value={input.phone_number || ''}
+              name="phone_number"
+              type="text"
+              placeholder=""
+            />
           </Form.Group>
         </Form.Row>
 
@@ -93,23 +139,35 @@ const ProfileInfo = () => {
           <Form.Group as={Col}>
             <Form.Label>Changer de mot de passe ?</Form.Label>
             <Form.Control
-              name="password" type="password" placeholder="Choisissez un mot de passe" />
+              onChange={handleInputChange}
+              value={input.password || ''}
+              name="password"
+              type="password"
+              placeholder="Choisissez un mot de passe"
+            />
           </Form.Group>
 
           <Form.Group as={Col}>
             <Form.Label>Confirmation du mot de passe</Form.Label>
             <Form.Control
-              name="passwordConfirmation" type="password" placeholder="Confirmez votre mot de passe" />
+              onChange={handleInputChange}
+              value={input.passwordConfirmation || ''}
+              name="passwordConfirmation"
+              type="password"
+              placeholder="Confirmez votre mot de passe"
+            />
           </Form.Group>
         </Form.Row>
 
-        <Button
-          id="button"
-          variant="primary"
-          type="submit">
-          Valider
-        </Button>
       </Form>
+
+      <Button
+        onClick={editProfile}
+        id="button"
+        variant="primary"
+        type="submit">
+        Valider
+      </Button>
 
     </Container>
   )
