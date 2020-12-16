992573bcfedf9009d7e794bb25fde9aeb5a38c8d
hsravo <hsravo1@gmail.com>
2020-12-16 16:03:12 +0100

    users_controller + routes fix

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,13 @@
 class Api::UsersController < Api::BaseController
 
-  before_action :find_user, only: %w[show]
+  before_action :find_user, only: [:show, :update]
 
   def show
+    render_jsonapi_response(@user)
+  end
+
+  def update
+    @user.update(user_params)
     render_jsonapi_response(@user)
   end
 
@@ -10,6 +15,18 @@ class Api::UsersController < Api::BaseController
 
   def find_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :first_name,
+      :last_name,
+      :username,
+      :phone_number,
+      :password,
+      :description
+      )
   end
 
 end
