e49c79e1e805122bf87364a3306d6d27bbcacc1c
Maxime CASTILLO <maxime.castillo@gmail.com>
2020-12-16 02:08:38 +0100

    Corrige routeur et nettoie packs/

--- app/javascript/packs/pages/Profile/index.jsx
+++ app/javascript/packs/pages/Profile/index.jsx
@@ -1,7 +1,6 @@
 import "./index.scss";
 import React from "react";
 import {
-  BrowserRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
@@ -12,33 +11,31 @@ import MyOffers from "./MyOffers";
 import NavProfile from "./NavProfile";
 import ProfileInfo from "./ProfileInfo";
 import NotFound from "../NotFound";
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import { Breadcrumb } from 'react-bootstrap';
 import { Container } from "react-bootstrap";
 
 const Profile = () => {
   return (
     <div id="body">
-        <Container >
-          <Breadcrumb className="breadcrumb">
-            <div id="Introductionphrase">Vous etes ici :</div>
-            <Breadcrumb.Item href="/" className="ml-2 breadcrumb_link">Accueil</Breadcrumb.Item>
-            <Breadcrumb.Item active className="color breadcrumb_link">Mon compte</Breadcrumb.Item>
-          </Breadcrumb>
-          <Router>
-          <h2>Page Profile en cours de construction...</h2>
-          <NavProfile />
-            <Switch>
-              <Route exact path="/mon-compte" component={ProfileInfo} />
-              <Route path="/mon-compte/mon-garage" component={Garage} />
-              <Route path="/mon-compte/mes-annonces" component={MyOffers} />
-              <Route path="/mon-compte/mes-favoris" component={Favorites} />
-              <Route path="/mon-compte/mes-reservations" component={MyBookings} />
-              <Route component={NotFound} />
-            </Switch>
-          </Router>
-        </Container>
+      <Container >
+        <Breadcrumb className="breadcrumb">
+          <div id="Introductionphrase">Vous etes ici :</div>
+          <Breadcrumb.Item href="/" className="ml-2 breadcrumb_link">Accueil</Breadcrumb.Item>
+          <Breadcrumb.Item active className="color breadcrumb_link">Mon compte</Breadcrumb.Item>
+        </Breadcrumb>
+        <h2>Page Profile en cours de construction...</h2>
+        <NavProfile />
+        <Switch>
+          <Route exact path="/mon-compte" component={ProfileInfo} />
+          <Route path="/mon-compte/mon-garage" component={Garage} />
+          <Route path="/mon-compte/mes-annonces" component={MyOffers} />
+          <Route path="/mon-compte/mes-favoris" component={Favorites} />
+          <Route path="/mon-compte/mes-reservations" component={MyBookings} />
+          <Route component={NotFound} />
+        </Switch>
+      </Container>
     </div>
   )
 }
 
-export default Profile
+export default Profile;
