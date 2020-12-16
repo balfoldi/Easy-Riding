331dde19899eb8fd09574cea60901d3636f117b3
Lucas <lucas.gautier@live.fr>
2020-12-16 01:15:06 +0100

    no more breadcrumb until tabs fix

--- app/javascript/packs/pages/Profile/index.jsx
+++ app/javascript/packs/pages/Profile/index.jsx
@@ -12,18 +12,12 @@ import MyOffers from "./MyOffers";
 import NavProfile from "./NavProfile";
 import ProfileInfo from "./ProfileInfo";
 import NotFound from "../NotFound";
-import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import { Container } from "react-bootstrap";
 
 const Profile = () => {
   return (
     <div id="body">
         <Container >
-          <Breadcrumb className="breadcrumb">
-            <div id="Introductionphrase">Vous etes ici :</div>
-            <Breadcrumb.Item href="/" className="ml-2 breadcrumb_link">Accueil</Breadcrumb.Item>
-            <Breadcrumb.Item active className="color breadcrumb_link">Mon compte</Breadcrumb.Item>
-          </Breadcrumb>
           <Router>
           <h2>Page Profile en cours de construction...</h2>
           <NavProfile />
