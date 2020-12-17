4c305f4e046f9326ac8a2c230f5385bea359c9a4
Lucas <lucas.gautier@live.fr>
2020-12-17 02:34:05 +0100

    Layout Page 404

--- app/javascript/packs/pages/NotFound/index.jsx
+++ app/javascript/packs/pages/NotFound/index.jsx
@@ -1,7 +1,22 @@
+import './index.scss';
 import React from "react";
+import { Container, Button, Form, Alert } from "react-bootstrap";
 
 const NotFound = () => {
-  return <p>404 not found</p>;
+  return (
+    <Container id="form-container">
+      <p id="intro">Erreur 404, il n'y a rien par ici {'\u00a0'}...</p>
+    <Row >
+      <Col></Col>
+        <Col xs={6}>
+          <Link to="/">
+          <button> Retour à la page d'accueil</button>
+          </Link>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  )
 };
 
 export default NotFound;
