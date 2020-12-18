166ac98b350a4f543aa0ff88291cd68b8ac1634b
balfoldi <balfoldi@yahoo.fr>
2020-12-18 03:23:28 +0100

    Some animations

--- app/javascript/packs/pages/Home/Jumbo/index.jsx
+++ app/javascript/packs/pages/Home/Jumbo/index.jsx
@@ -1,22 +1,24 @@
-import './index.scss';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Jumbotron, Button, Container} from "react-bootstrap";
+import "./index.scss";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Jumbotron, Button, Container } from "react-bootstrap";
 import SearchMap from "./SearchMap";
 
 const Jumbo = () => {
   return (
-      <Jumbotron fluid id="jumbotron">
-        <div id="info">
-          <p id="presentation">Visiter la France sur la moto de mes rêves{'\u00a0'}?</p>
-          <Link to="/annonces" id="home-link">
-            <Button className="ButtonHomepage" variant="contained" id="home-button"><span>C'est parti</span></Button>
-          </Link>
-        </div>
-        <div id="map">
-          <SearchMap />
-        </div>
-      </Jumbotron>
+    <Jumbotron fluid id="jumbotron">
+      <div id="info">
+        <p id="presentation">Visiter la France sur la moto de mes rêves{"\u00a0"}?</p>
+        <Link to="/annonces" id="home-link">
+          <Button className="ButtonHomepage" variant="contained" id="home-button">
+            <span>C'est parti</span>
+          </Button>
+        </Link>
+      </div>
+      <div id="map">
+        <SearchMap />
+      </div>
+    </Jumbotron>
   );
 };
 
