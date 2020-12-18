d0e8a6a4ba09245b5a1449a804a0dd244bbea61f
Lucas <lucas.gautier@live.fr>
2020-12-18 20:41:48 +0100

    header image back fix

--- app/javascript/packs/components/layout/HeaderImage/index.jsx
+++ app/javascript/packs/components/layout/HeaderImage/index.jsx
@@ -6,14 +6,8 @@ import { ScrollTo } from "react-scroll-to";
 const HeaderImage = () => {
   return (
     <section className="HeaderImage">
-      <Row className="d-flex flex-column mb-3">
-        <Col div="Box_en_haut">
-          <div id="Titre_Homepage">Easy Riding</div>
-          <div id="Headline_Homepage">Louez une moto,
-          partez à l'aventure.</div>
-        </Col>
-        <Col div="Box_en_bas" md="auto">
-        <p> BAS</p>
+      <Row className="justify-content-md-center">
+        <Col md="auto">
           <ScrollTo>
             {({ scroll }) => (
               <div className="arrow"
