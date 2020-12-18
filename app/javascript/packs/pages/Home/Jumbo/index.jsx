c99944aef8518bbc903534bf65b6888141e9e870
Maxime CASTILLO <maxime.castillo@gmail.com>
2020-12-18 03:25:47 +0100

    Hooks, améliore ProfileInfo, PrivateRoute, cas 401

--- app/javascript/packs/pages/Home/Jumbo/index.jsx
+++ app/javascript/packs/pages/Home/Jumbo/index.jsx
@@ -1,22 +1,25 @@
 import './index.scss';
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Jumbotron, Button, Container} from "react-bootstrap";
+import { Link, useLocation } from 'react-router-dom';
+import { Jumbotron, Button, Alert } from "react-bootstrap";
 import SearchMap from "./SearchMap";
 
 const Jumbo = () => {
+  const location = useLocation();
+
   return (
-      <Jumbotron fluid id="jumbotron">
-        <div id="info">
-          <p id="presentation">Visiter la France sur la moto de mes rêves{'\u00a0'}?</p>
-          <Link to="/annonces" id="home-link">
-            <Button className="ButtonHomepage" variant="contained" id="home-button"><span>C'est parti</span></Button>
-          </Link>
-        </div>
-        <div id="map">
-          <SearchMap />
-        </div>
-      </Jumbotron>
+    <Jumbotron fluid id="jumbotron">
+      {location?.state?.message && <Alert variant="warning">{location.state.message}</Alert>}
+      <div id="info">
+        <p id="presentation">Visiter la France sur la moto de mes rêves{'\u00a0'}?</p>
+        <Link to="/annonces" id="home-link">
+          <Button className="ButtonHomepage" variant="contained" id="home-button"><span>C'est parti</span></Button>
+        </Link>
+      </div>
+      <div id="map">
+        <SearchMap />
+      </div>
+    </Jumbotron>
   );
 };
 
--- app/javascript/packs/pages/Profile/ProfileInfo/index.jsx
+++ app/javascript/packs/pages/Profile/ProfileInfo/index.jsx
@@ -1,71 +1,48 @@
 import "./index.scss";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Card, Button } from "react-bootstrap";
-//import authStore from "../../../stores/Auth";
+import authStore from "../../../stores/Auth";
 import { observer } from "mobx-react";
-import Cookies from "js-cookie";
 import ProfileModal from "./ProfileModal";
-import jwt_decode from "jwt-decode";
 import Logo from "./default_avatar.png";
+import useGetData from "../../../hooks/useGetData";
 
 const ProfileInfo = () => {
-  const userToken = Cookies.get("EasyRidingUserToken");
-  const [userData, setUserData] = useState({});
-  const [show, setShow] = useState(false);
-  //const { user } = authStore;
-  const user = { id: '' };
-  user.id = jwt_decode(userToken).sub;
+  const reloadTick = useRef(0);
+  const { user, userID } = authStore;
+  const { isLoading, data: userData } = useGetData(`users/${userID}`, reloadTick.current);
+  const [isShown, setIsShown] = useState(false);
 
   const showModal = () => {
-    setShow(true);
+    setIsShown(true);
   }
-
-  const loadProfile = () => {
-    fetch(`/api/users/${user.id}`, {
-      method: 'get',
-      headers: {
-        'Authorization': `Bearer ${userToken}`
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setUserData(data);
-      })
-      .catch((error) => console.error(error));
-  }
-
-  useEffect(() => {
-    loadProfile();
-  }, [])
 
   return (
-    <Card style={{ width: '18rem' }} id="card-profile">
-      <Card.Img id="img-circle" variant="top" src={Logo} />
-      <Card.Body id="card-body">
-        <Card.Title>
-        {userData.username && userData.first_name && userData.last_name ? <p>{userData.first_name} {userData.last_name}</p> : userData.email}
-        </Card.Title>
-        <Card.Text id="description">
-        {userData.username && userData.first_name && userData.last_name ?
-          <p>({userData.username})</p>
-          : <p>Complétez vos informations de profil !</p>}
-        <p>{userData.description ? userData.description : `Pas de description`}</p>
-        </Card.Text>
-      </Card.Body>
-      <Button id="card-button" variant="outline-dark" onClick={showModal}>
-        Modifier le profil
-      </Button>
-      <ProfileModal
-        user={user}
-        userToken={userToken}
-        show={show}
-        setShow={setShow}
-        showModal={showModal}
-        userData={userData}
-        setUserData={setUserData}
-        loadProfile={loadProfile}
-      />
-    </Card>
+    <>
+      {(isLoading || !userData) ? <p>Loading</p> : (
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={Logo} />
+          <Card.Body>
+            <Card.Title>{userData.first_name} {userData.last_name} ({userData.username})</Card.Title>
+            <Card.Text>
+              {userData.description === "" ? <p>Pas de description</p> : userData.description}
+            </Card.Text>
+          </Card.Body>
+          <Button id="cardButton" onClick={showModal}>
+            Modifier le profil
+          </Button>
+          <ProfileModal
+            userID={userID}
+            isShown={isShown}
+            setIsShown={setIsShown}
+            userData={userData}
+            onSaved={() => {
+              reloadTick.current = reloadTick.current + 1
+            }}
+          />
+        </Card>
+      )}
+    </>
   )
 }
 
