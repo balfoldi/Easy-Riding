74bf68ba6b43e43d7ef8852d6e50ae33ccd4fb9d
hsravo <hsravo1@gmail.com>
2020-12-18 22:24:36 +0100

    garage ok

--- app/javascript/packs/pages/Profile/index.jsx
+++ app/javascript/packs/pages/Profile/index.jsx
@@ -36,7 +36,7 @@ const Profile = () => {
             {key === "mes-annonces" && <MyOffers />}
           </div>
         </Tab>
-        <Tab eventKey="reservations-recues" title="Réservation reçues">
+        <Tab eventKey="reservations-recues" title="Réservations reçues">
           <div id="tab-body">
             {key === "reservations-recues" && <MyBookings consumer={"received"} />}
           </div>
